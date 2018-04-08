a4c605cfb27aed980b15e7134810c2d84f1074b9
Iain Kirkpatrick <kirkpatrick.iain@gmail.com>
2018-04-08 19:07:47 +1200

    when creating type, user must state which topic it belongs to, create it in that topic folder

--- commands/createType.js
+++ commands/createType.js
@@ -16,39 +16,62 @@ const templateOptions = keys(typesTemplates)
 - Creates a type file
 */
 
+// TODO: IK: change topicName here to fileName or something similar, think it makes more sense
 module.exports = function createType ({ typeName, folderName, folderPath, topicName, template }) {
-
   return function (cb) {
     if (typeName && !includes(templateOptions, typeName)) {
       cb(new Error(`${typeName} is an unsupported type: please pick one of ${templateOptions.join(', ')}`))
     } else if (!typeName) {
-      inquirer.prompt([{
-        type: 'list',
-        name: 'input',
-        message: "Which type?",
-        choices: templateOptions
-      }])
-      .then(function (answers) {
-        typeName = answers.input
-        templateFn = typesTemplates[answers.input]
-        // TODO: IK: ideally ask the user if this type is for an existing topic, which would let us figure out the name and the dir, maybe scan some levels of lower folders for matching names or something
-        return inquirer.prompt([{
-          type: 'input',
-          name: 'input',
-          message: `What's the name of the ${answers.input}?`,
-          validate: (answer) => answer.length >= 1
-        }])
-      })
-      .then(function (answers) {
-        topicName = answers.input
-        folderPath = process.cwd() // TODO: IK: think about nice ways to prompt the user to pick a dir,
-        createType({
-          typeName,
-          folderName: topicName,
-          folderPath,
-          topicName,
-          template: templateFn(topicName)
-        })(cb)
+      const startDir = process.cwd()
+
+      fs.readdir(startDir, function (err, files) {
+        if (err) cb(err)
+
+        const topicPaths = files
+        .map((name) => path.join(startDir, name))
+        .filter((file) => fs.lstatSync(file).isDirectory()) // TODO: IK: use async lstat?
+        .filter((dir) => {
+          const base = path.basename(dir)
+          return base !== 'config' && base !== 'db' && base !== 'node_modules'
+        })
+        const topics = topicPaths.map((topicPath) => path.basename(topicPath))
+
+        inquirer.prompt([
+          {
+            type: 'list',
+            name: 'whichType',
+            message: "Which type?",
+            choices: templateOptions
+          },
+          {
+            type: 'list',
+            name: 'whichTopic',
+            message: "Which topic? (if the choices don't make sense, make sure you are running create-dogstack-app from your app's root directory)",
+            choices: topics
+          }
+        ])
+        .then(function (answers) {
+          typeName = answers.whichType
+          templateFn = typesTemplates[answers.whichType]
+          folderName = typeName === 'dux' ? typeName : typeName + 's' // pluralize folder names
+          folderPath = path.join(startDir, answers.whichTopic, folderName)
+          return inquirer.prompt([{
+            type: 'input',
+            name: 'fileName',
+            message: `What's the name of the ${answers.whichType}?`,
+            validate: (answer) => answer.length >= 1
+          }])
+        })
+        .then(function (answers) {
+          topicName = answers.fileName
+          createType({
+            typeName,
+            folderName,
+            folderPath,
+            topicName,
+            template: templateFn(topicName)
+          })(cb)
+        })
       })
     } else {
       const fileName = typeName === 'getter' ? `get${capitalize(topicName)}Props.js` : `${topicName}.js`
